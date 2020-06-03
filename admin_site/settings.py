f87fd6430dd6bb5fc288a1191308fe98ed48daef
Zulfiqar <zusoomro@seas.upenn.edu>
2020-06-03 00:27:59 -0400

    Make it better

--- admin_site/settings.py
+++ admin_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%o^hho9b*u(v1inn2v%n&75&^vv%n9s1j_px#b&x9_rei=r5j#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
